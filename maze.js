aea02caa0a4360d1f1ff5fc2554de3b3ac82df19
fattah-ab <fattaha.12@gmail.com>
2021-01-07 00:20:06 +0700

    push result

---
+++ maze.js
@@ -0,0 +1,28 @@
+function zigZag(s) { 
+    if ((s + 1) % 4 != 0 || s < 0) {// cek kondisi s = positif dan s = 4n - 1 (n adalah positif integer) 
+    return 'Error: S harus bilangan positif dan S harus dapat dinyatakan dalam 4n - 1' 
+    }
+    let maze = '';  // membuat container 
+    
+    for (let i = 0; i < s; i++) { 
+        maze += '@' // bangun tembok paling kiri 
+        
+        for (let j = 0; j < s - 1; j++) { 
+            if (i % 2 == 0) { 
+                if ((i % 4 == 0 && j == 0) || (i % 4 != 0 && j == s - 2)) {
+                    maze += ' ' // menentukan pintu untuk ke jalur berikutnya di kanan atau kiri
+                } else {
+                    maze += '@' // membangun tembok pembatas 
+                }
+            } else {
+                maze += ' ' // membuat jalan 
+            }
+        } 
+        maze += '@\n' // bangun tembok paling kanan dan pindah ke jalur berikutnya 
+        
+    } 
+    return maze
+
+} 
+
+console.log(zigZag(15));
